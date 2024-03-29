40055084c7f4603f80889f303129e93bdae3a61f
spo <sebastien.porquet@atomicweb.fr>
2024-03-29 15:03:31 +0100

    #2 - Cleanup config.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -11,11 +11,11 @@ return [
     | framework when an event needs to be broadcast. You may set this to
     | any of the connections defined in the "connections" array below.
     |
-    | Supported: "pusher", "redis", "log", "null"
+    | Supported: "reverb", "pusher", "ably", "redis", "log", "null"
     |
     */
 
-    'default' => env('BROADCAST_DRIVER', 'pusher'),
+    'default' => env('BROADCAST_CONNECTION', 'null'),
 
     /*
     |--------------------------------------------------------------------------
@@ -23,44 +23,60 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may define all of the broadcast connections that will be used
-    | to broadcast events to other systems or over websockets. Samples of
+    | to broadcast events to other systems or over WebSockets. Samples of
     | each available type of connection are provided inside this array.
     |
     */
 
     'connections' => [
 
-        'pusher' => [
-            'driver'  => 'pusher',
-            'key'     => env('PUSHER_APP_KEY'),
-            'secret'  => env('PUSHER_APP_SECRET'),
-            'app_id'  => env('PUSHER_APP_ID'),
-            'options' => [
-                'cluster'      => env('PUSHER_APP_CLUSTER'),
-                'useTLS'       => env('PUSHER_APP_USE_TLS', false),
-                'encrypted'    => true,
-                'host'         => env('PUSHER_APP_HOST', '127.0.0.1'),
-                'port'         => 6001,
-                'scheme'       => env('PUSHER_APP_SCHEME', 'http'),
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => env('PUSHER_APP_CURLOPT_SSL_VERIFYHOST', 0),
-                    CURLOPT_SSL_VERIFYPEER => env('PUSHER_APP_CURLOPT_SSL_VERIFYPEER', 0),
-                ],
+            'reverb' => [
+                    'driver'         => 'reverb',
+                    'key'            => env('REVERB_APP_KEY'),
+                    'secret'         => env('REVERB_APP_SECRET'),
+                    'app_id'         => env('REVERB_APP_ID'),
+                    'options'        => [
+                            'host'   => env('REVERB_HOST'),
+                            'port'   => env('REVERB_PORT', 443),
+                            'scheme' => env('REVERB_SCHEME', 'https'),
+                            'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                    ],
+                    'client_options' => [
+                        // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                    ],
             ],
-        ],
 
-        'redis' => [
-            'driver'     => 'redis',
-            'connection' => 'default',
-        ],
+            'pusher' => [
+                    'driver'  => 'pusher',
+                    'key'     => env('PUSHER_APP_KEY'),
+                    'secret'  => env('PUSHER_APP_SECRET'),
+                    'app_id'  => env('PUSHER_APP_ID'),
+                    'options' => [
+                            'cluster'      => env('PUSHER_APP_CLUSTER'),
+                            'useTLS'       => env('PUSHER_APP_USE_TLS', false),
+                            'encrypted'    => true,
+                            'host'         => env('PUSHER_APP_HOST', '127.0.0.1'),
+                            'port'         => 6001,
+                            'scheme'       => env('PUSHER_APP_SCHEME', 'http'),
+                            'curl_options' => [
+                                    CURLOPT_SSL_VERIFYHOST => env('PUSHER_APP_CURLOPT_SSL_VERIFYHOST', 0),
+                                    CURLOPT_SSL_VERIFYPEER => env('PUSHER_APP_CURLOPT_SSL_VERIFYPEER', 0),
+                            ],
+                    ],
+            ],
 
-        'log' => [
-            'driver' => 'log',
-        ],
+            'ably' => [
+                    'driver' => 'ably',
+                    'key'    => env('ABLY_KEY'),
+            ],
 
-        'null' => [
-            'driver' => 'null',
-        ],
+            'log' => [
+                    'driver' => 'log',
+            ],
+
+            'null' => [
+                    'driver' => 'null',
+            ],
 
     ],
 
--- config/reverb.php
+++ config/reverb.php
@@ -28,20 +28,20 @@ return [
 
     'servers' => [
 
-        'reverb' => [
-            'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
-            'port' => env('REVERB_SERVER_PORT', 8080),
-            'hostname' => env('REVERB_HOST'),
-            'options' => [
-                'tls' => [],
+            'reverb' => [
+                    'host'                  => env('REVERB_SERVER_HOST', '0.0.0.0'),
+                    'port'                  => env('REVERB_SERVER_PORT', 8080),
+                    'hostname'              => env('REVERB_HOST'),
+                    'options'               => [
+                            'tls' => [],
+                    ],
+                    'max_request_size'      => env('REVERB_MAX_REQUEST_SIZE', 10_000),
+                    'scaling'               => [
+                            'enabled' => env('REVERB_SCALING_ENABLED', false),
+                            'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
+                    ],
+                    'pulse_ingest_interval' => env('REVERB_PULSE_INGEST_INTERVAL', 15),
             ],
-            'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
-            'scaling' => [
-                'enabled' => env('REVERB_SCALING_ENABLED', false),
-                'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
-            ],
-            'pulse_ingest_interval' => env('REVERB_PULSE_INGEST_INTERVAL', 15),
-        ],
 
     ],
 
@@ -58,24 +58,24 @@ return [
 
     'apps' => [
 
-        'provider' => 'config',
+            'provider' => 'config',
 
-        'apps' => [
-            [
-                'key' => env('REVERB_APP_KEY'),
-                'secret' => env('REVERB_APP_SECRET'),
-                'app_id' => env('REVERB_APP_ID'),
-                'options' => [
-                    'host' => env('REVERB_HOST'),
-                    'port' => env('REVERB_PORT', 443),
-                    'scheme' => env('REVERB_SCHEME', 'https'),
-                    'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
-                ],
-                'allowed_origins' => ['*'],
-                'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
-                'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
+            'apps' => [
+                    [
+                            'key'              => env('REVERB_APP_KEY'),
+                            'secret'           => env('REVERB_APP_SECRET'),
+                            'app_id'           => env('REVERB_APP_ID'),
+                            'options'          => [
+                                    'host'   => env('REVERB_HOST'),
+                                    'port'   => env('REVERB_PORT', 443),
+                                    'scheme' => env('REVERB_SCHEME', 'https'),
+                                    'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                            ],
+                            'allowed_origins'  => ['*'],
+                            'ping_interval'    => env('REVERB_APP_PING_INTERVAL', 60),
+                            'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
+                    ],
             ],
-        ],
 
     ],
 
