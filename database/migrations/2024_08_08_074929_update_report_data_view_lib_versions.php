78c3046153517ce04f6037bd022b462430289968
spo <sebastien.porquet@atomicweb.fr>
2024-08-08 09:52:15 +0200

    #5 - update chartjs version.

---
+++ database/migrations/2024_08_08_074929_update_report_data_view_lib_versions.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of RootDB.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * AUTHORS
+ * PORQUET Sébastien <sebastien.porquet@ijaz.fr>
+ */
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        DB::statement("update report_data_view_lib_versions set major_version = '4.x', version = '4.4.3' where id = 3");
+
+    }
+};
