80762698f51958865bba8b1c51ee8bc29cc13dcd
spo <sebastien.porquet@atomicweb.fr>
2024-09-04 14:35:09 +0200

    RootDBApp/rootdb#3 - Apache ECharts - generate image instead of png, by default.

--- storage/app/templates/data_view_lib_versions/9/bar_register.js
+++ storage/app/templates/data_view_lib_versions/9/bar_register.js
@@ -5,6 +5,6 @@ if(refDiv.current) {
     if(!chart[DATA_VIEW_JS_ID]) {
 
         var chartDom[DATA_VIEW_JS_ID] = document.getElementById(refDiv.current.id);
-        chart[DATA_VIEW_JS_ID] = ec.init(chartDom[DATA_VIEW_JS_ID], null, { renderer: 'svg' });
+        chart[DATA_VIEW_JS_ID] = ec.init(chartDom[DATA_VIEW_JS_ID], null, { renderer: 'png' });
     }
 }
