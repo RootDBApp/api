08eff2b34993e2c98aefc1076d8e40cb6c4d9226
PORQUET Sébastien <sebastien.porquet@atomicweb.fr>
2024-10-11 15:56:34 +0200

    RootDBApp/api#10 - new column in report_data_views to handle dynamic modules imports.

--- app/Http/Resources/ReportDataView.php
+++ app/Http/Resources/ReportDataView.php
@@ -27,6 +27,7 @@ use App\Http\Resources\ReportDataViewJs as ReportDataViewJsResource;
 use App\Models\RoleGrants;
 use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
+use JShrink\Minifier;
 
 /** @mixin \App\Models\ReportDataView */
 class ReportDataView extends JsonResource
@@ -34,58 +35,61 @@ class ReportDataView extends JsonResource
     public function toArray($request): array
     {
         $loggedUserHasRoleDev = User::searchIfLoggedUserHasUiRoleGrant(RoleGrants::PERMISSION_EDIT, RoleGrants::RESOURCE_REPORT_DATA_VIEW);
+        $json_runtime_configuration = (!$loggedUserHasRoleDev && !is_null($this->js_runtime_configuration)) ? base64_encode(gzcompress(Minifier::minify($this->js_init))) : $this->js_init;
 
         return [
-            'id'                              => $this->id,
-            'by_chunk'                        => $this->by_chunk,
-            'chunk_size'                      => $this->chunk_size,
-            'created_at'                      => $this->created_at,
-            'name'                            => $this->name,
-            'description'                     => $this->description,
-            'description_display_type'        => (int)$this->description_display_type,
-            'is_visible'                      => $this->is_visible,
-            'on_queue'                        => $this->on_queue,
-            'query'                           => $this->when($loggedUserHasRoleDev,
+            'id'                                  => $this->id,
+            'by_chunk'                            => $this->by_chunk,
+            'chunk_size'                          => $this->chunk_size,
+            'created_at'                          => $this->created_at,
+            'name'                                => $this->name,
+            'description'                         => $this->description,
+            'description_display_type'            => (int)$this->description_display_type,
+            'is_visible'                          => $this->is_visible,
+            'on_queue'                            => $this->on_queue,
+            'query'                               => $this->when($loggedUserHasRoleDev,
                 function () {
                     return $this->query;
                 }
             ),
-            'max_width'                       => $this->max_width,
-            'position'                        => $this->position,
-            'report'                          => $this->when(
+            'max_width'                           => $this->max_width,
+            'position'                            => $this->position,
+            'report'                              => $this->when(
                 $request->get('report-id') === 0 && $loggedUserHasRoleDev,
                 function () {
                     return ReportResource::make($this->report);
                 }
             ),
-            'report_id'                       => $this->when(
+            'report_id'                           => $this->when(
                 $request->get('report-id') === 0,
                 function () {
                     return $this->report_id;
                 }
             ),
-            'report_data_view_js'             => $this->when(
+            'report_data_view_js'                 => $this->when(
                 $request->get('report-id') >= 1,
                 function () {
                     return ReportDataViewJsResource::make($this->reportDataViewJs);
                 }
             ),
-            'report_data_view_js_id'          => $this->when(
+            'report_data_view_js_id'              => $this->when(
                 $request->get('report-id') >= 1 && $loggedUserHasRoleDev,
                 function () {
                     return $this->reportDataViewJs->id;
                 }
             ),
-            'report_data_view_lib_version'    => $this->when(
+            'report_data_view_lib_version'        => $this->when(
                 $request->get('report-id') >= 1 && $loggedUserHasRoleDev,
                 function () {
                     return ReportDataViewLibVersionResource::make($this->reportDataViewLibVersion);
                 }
             ),
-            'report_data_view_lib_version_id' => $this->report_data_view_lib_version_id,
-            'title'                           => $this->title,
-            'type'                            => (int)$this->type,
-            'updated_at'                      => $this->updated_at,
+            'report_data_view_lib_version_id'     => $this->report_data_view_lib_version_id,
+            'json_runtime_configuration'          => $json_runtime_configuration,
+            'json_runtime_configuration_minified' => !$loggedUserHasRoleDev,
+            'title'                               => $this->title,
+            'type'                                => (int)$this->type,
+            'updated_at'                          => $this->updated_at,
         ];
     }
 }
---
+++ database/migrations/2024_10_11_124312_update_report_data_views.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('report_data_views', function (Blueprint $table) {
+            $table->addColumn('json', 'json_runtime_configuration')->default('{}')->comment(' We put here everything the view will require to run on the frontend side. For instance, the dynamic modules imports for JS code.');
+        });
+    }
+};
